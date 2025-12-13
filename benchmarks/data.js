302ba71e30ac5539b100494e8403b6edb68d7b39
github-action-benchmark <github@users.noreply.github.com>
2025-12-13 13:14:36 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for c8b140935b419b6131937c074c2a987e82e2eaa3

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765626191551,
+  "lastUpdate": 1765631676797,
   "repoUrl": "https://github.com/rcnsnr/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/rcnsnr/n8n-mcp/commit/739a04c35c1c2abb35cb3bda7132769bea0b69b0"
         },
         "date": 1765626190783,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orcunsener@gmail.com",
+            "name": "Orçun Şener",
+            "username": "rcnsnr"
+          },
+          "committer": {
+            "email": "orcunsener@gmail.com",
+            "name": "Orçun Şener",
+            "username": "rcnsnr"
+          },
+          "distinct": true,
+          "id": "c8b140935b419b6131937c074c2a987e82e2eaa3",
+          "message": "fix(docker-compose.n8n): pass MCP_PORT env for healthcheck on custom ports (docker-compose.n8n.yml)",
+          "timestamp": "2025-12-13T16:12:59+03:00",
+          "tree_id": "a4dbaa65709cb7ddbaf325420ea34369a8ffacaa",
+          "url": "https://github.com/rcnsnr/n8n-mcp/commit/c8b140935b419b6131937c074c2a987e82e2eaa3"
+        },
+        "date": 1765631676533,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
