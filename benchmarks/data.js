b2a71b6e56c2c9c97176aa24d36a8900da18bdaf
github-action-benchmark <github@users.noreply.github.com>
2025-12-13 11:43:11 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 739a04c35c1c2abb35cb3bda7132769bea0b69b0

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1765626191551,
+  "repoUrl": "https://github.com/rcnsnr/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "orcunsener@gmail.com",
+            "name": "Orçun Şener",
+            "username": "rcnsnr"
+          },
+          "committer": {
+            "email": "orcunsener@gmail.com",
+            "name": "Orçun Şener",
+            "username": "rcnsnr"
+          },
+          "distinct": true,
+          "id": "739a04c35c1c2abb35cb3bda7132769bea0b69b0",
+          "message": "merge: sync upstream/main; resolve package-lock.json; keep anakin ignored",
+          "timestamp": "2025-12-13T14:23:28+03:00",
+          "tree_id": "2d84ef7a37a7607e915d68164ce57974f9a309ee",
+          "url": "https://github.com/rcnsnr/n8n-mcp/commit/739a04c35c1c2abb35cb3bda7132769bea0b69b0"
+        },
+        "date": 1765626190783,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
